4236efca621e0f235c28b477bac1bfdc1c67e458
Azim Baghadiya <azim-work@outlook.com>
2025-08-22 19:17:18 -0400

    feat/be: move db connection details to env vars

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,11 @@
 from fastapi import FastAPI
 import psycopg2
 from psycopg2 import OperationalError
+from dotenv import load_dotenv
+import os
 
+# load environment variables
+load_dotenv()
 
 app = FastAPI()
 
@@ -15,13 +19,14 @@ async def root():
 
 
 def can_reach_db():
+
     try:
         conn = psycopg2.connect(
-            dbname="budgeto",
-            user="postgres",
-            password="password",
-            host="localhost",
-            port="5432",
+            dbname=os.getenv("DB_NAME"),
+            user=os.getenv("DB_USER"),
+            password=os.getenv("DB_PASSWORD"),
+            host=os.getenv("DB_HOST"),
+            port=os.getenv("DB_PORT", "5432"),  # Default PostgreSQL port
         )
         conn.close()
         return True
