9effb0cbb81d22f46022ee314e5cce21b2cab591
Azim Baghadiya <azim-work@outlook.com>
2025-08-21 23:52:39 -0400

    chore: move all the fe code in frontend

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,16 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
-import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import { SettingsProvider } from "./context/SettingsContext.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <SettingsProvider>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <App />
     </SettingsProvider>
   </React.StrictMode>
 );
