98df0063d2b60c51b266c4b7f1a643d9c0843532
Azim Baghadiya <azim-work@outlook.com>
2025-08-29 18:00:39 -0400

    feat/be: DB_URL as one env var

--- backend/config.py
+++ backend/config.py
@@ -4,9 +4,7 @@ from typing import List
 
 load_dotenv()
 
-DATABASE_URL = (
-    f"postgresql://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}"
-    f"@{os.getenv('DB_HOST')}:{os.getenv('DB_PORT')}/{os.getenv('DB_NAME')}"
-)
+DATABASE_URL = os.getenv("DATABASE_URL")
+
 
 CORS_ORIGINS: List = os.getenv("CORS_ORIGINS", "").split(",")
